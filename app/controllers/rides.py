7561d0a4d1c69d71057fc0e7580b2980b0fe0f5b
santiagoBassi <santiagobassi21@gmail.com>
2024-09-23 17:14:47 -0300

    Update: Package and People searches are now merged into rideSearch

--- app/controllers/rides.py
+++ app/controllers/rides.py
@@ -20,14 +20,9 @@ router = APIRouter(
 )
 
 #buscar viajes para personas
-@router.get("/people") 
-async def get_people_ride(city_from: str, city_to: str, date: date, people:  int ,  medium_packages: int, large_packages: int):
-    return rides.get_people_ride(city_from, city_to, date, people,  medium_packages, large_packages) 
-
-#esto es para buscar viajes para paquetes
-@router.get("/package")
-async def get_package_ride(city_from: str, city_to: str, date: date, small_packages: int , medium_packages: int , large_packages: int ):
-    return rides.get_package_ride(city_from, city_to, date, small_packages, medium_packages, large_packages)
+@router.get("/search") 
+async def get_ride(city_from: str, city_to: str, date: date, people:  int , small_packages: int,  medium_packages: int, large_packages: int):
+    return rides.get_ride(city_from, city_to, date, people, small_packages,  medium_packages, large_packages) 
 
 
 #retorna los precios y -1 si no se encontro alguna de las ciudades
@@ -86,37 +81,7 @@ async def create_ride(ride: RideCreate, price: PriceSet, plate: str , current_us
 
     return 0
 
-##VEEEEEEEEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
-
-
-
-
-# @router.post("/create/detail")#tiene que estar logueado
-# async def create_ride(ride: RideCreate, price: PriceSet, plate: str , current_user: User = Depends(get_current_active_user), db: Session = Depends(get_db)):#quien es el driver, ver manejo de sesiones.El driver es el usuario logueado en ese momento
-#     #Check if user is a driver, check that the car is from the user
-#     driver = db.query(Drivers).filter(Drivers.user_id == current_user.user_id).first()
-#     if not driver:
-         
-#          return {"User is not a driver"} # aca tenemos que ver de tirar un error http
-
-#     driver_id = driver.driver_id
-#     check_plate = db.query(Drives).filter(Drives.plate == plate, Drives.driver_id == driver_id).first()
-#     if not check_plate:
-        
-#         return {"Car does not belong to the driver"} # aca tenemos que ver de tirar un error http
-
-    
-
-
-#     ans = rides.create_ride(ride, price, plate, driver_id)
-
-#     if ans == -1:
-#         return {"Invalid city"} # ver de tirar una excepcion http
-    
-#     return 0;#Todo bien 
-
-   
-
+##VEEEEEEEEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
 
 # #esto sirve para que el formulario de edicion de un viaje nos pida todos los datos y despues los edite y nos llamen al edit_ride
 # @router.get("/ride{ride_id}") #tiene que estar logueado y el viaje debe ser suyo
